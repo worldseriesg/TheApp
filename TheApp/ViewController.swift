502309872c8bf92051432fd3c4025c515de64f8f
Gary <forsale@Garys-MBP.home>
2017-08-28 02:34:38 -0400

    Boolean life

--- TheApp/ViewController.swift
+++ TheApp/ViewController.swift
@@ -11,29 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var theLabel: UILabel!
-
+    
     @IBOutlet weak var text1: UITextField!
-
+    
     @IBOutlet weak var text2: UITextField!
     
     var TapCount = 0
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-      theLabel.text = "The total is... \((Double(text1.text!)! + Double(text2.text!)!))"
-
+        let addition = false
+        
+        if addition { theLabel.text = "The total is... \((Double(text1.text!)! + Double(text2.text!)!))"
+        } else { theLabel.text = "The total is... \((Double(text1.text!)! - Double(text2.text!)!))"
         }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
